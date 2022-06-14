6f2795b6bfd97fdccd5b3e45a3d9cfa778b694c8
rauliathos <rauliathos@gmail.com>
2022-06-14 15:22:12 +0000

    all we've done today

---
+++ FlaskDay2/databaseLayer/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ FlaskDay2/flask_app_ex/application/models.py
@@ -0,0 +1,5 @@
+from application import db
+
+class Human(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(20), nullable=False)
